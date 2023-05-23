4d18bba0405b495d0fe4dadaefdc92b147d57f5e
gogumaC(mac mini) <kimu2371@naver.com>
2023-05-23 12:08:29 +0900

    feat(Home): Create Record prototype

--- app/src/main/java/com/gogumac/climbup/MainActivity.kt
+++ app/src/main/java/com/gogumac/climbup/MainActivity.kt
@@ -6,11 +6,18 @@ import android.widget.CalendarView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.Card
@@ -20,7 +27,11 @@ import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -28,6 +39,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.gogumac.climbup.ui.theme.ClimbUpTheme
+import java.time.DayOfWeek
 import java.util.Date
 
 class MainActivity : ComponentActivity() {
@@ -40,9 +52,9 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun ClimbUpApp(modifier: Modifier=Modifier){
+        //var year by rememberSaveable { mutableStateOf<Int>(0) }
         ClimbUpTheme {
             // A surface container using the 'background' color from the theme
-
             val onDateChanged:(CalendarView,Int,Int,Int)->Unit={view,year,month,dayOfWeek->
                 Log.d("checkcheck","$year $month $dayOfWeek")
             }
@@ -53,6 +65,7 @@ class MainActivity : ComponentActivity() {
                 Column() {
                     Summary()
                     ClimbupCalendar(onDateChanged=onDateChanged)
+                    ClimbingRecord(date = Date(), records = listOf("LV1","LV2","LV3","LV4","LV5","LV6","LV7","LV8","LV9"))
                 }
 
 
@@ -69,9 +82,7 @@ class MainActivity : ComponentActivity() {
             views.date
         }
         Card(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(top=15.dp, start = 15.dp,end=15.dp),
+            modifier = modifier,
             colors = CardDefaults.cardColors(
                 containerColor = MaterialTheme.colorScheme.primaryContainer
             ),
@@ -83,6 +94,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun Summary(modifier: Modifier=Modifier){
         BasicCard(
+            modifier=modifier.padding(top = 15.dp, start = 15.dp, end = 15.dp)
         ) {
             Column(modifier = modifier.padding(10.dp)) {
                 Text(stringResource(R.string.home1))
@@ -103,8 +115,14 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun ClimbupCalendar(modifier: Modifier=Modifier,onDateChanged:(CalendarView,Int,Int,Int)->Unit){
-        BasicCard(modifier=modifier.fillMaxWidth()) {
+    fun ClimbupCalendar(
+        modifier: Modifier=Modifier,
+        onDateChanged:(CalendarView,Int,Int,Int)->Unit
+    ){
+        BasicCard(
+            modifier=modifier.fillMaxWidth()
+                    .padding(top = 15.dp, start = 15.dp, end = 15.dp)
+        ) {
             AndroidView(
                 { CalendarView(it) },
                 modifier=modifier.fillMaxWidth(),
@@ -117,7 +135,58 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @Composable
+    fun ClimbingRecord(
+        modifier: Modifier=Modifier,
+        date: Date=Date(),
+        records:List<String> = listOf()
+    ){
+        Column(
+            modifier=modifier.padding( start = 15.dp, end = 15.dp)
+        ) {
+            Row(
+                modifier= modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 5.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
 
+                ) {
+                Text(
+                    text=String.format("%tF",date),
+                    modifier=Modifier.align(Alignment.Bottom)
+                )
+                TextButton(
+                    modifier = modifier
+                        .wrapContentWidth()
+                        .wrapContentHeight()
+                        .align(Alignment.Bottom ),
+                    contentPadding=PaddingValues(),
+
+                    onClick = {}
+                ) {
+                    Text(
+                        stringResource(id = R.string.detail),
+                        modifier=modifier.align(Alignment.Bottom)
+                    )
+                }
+            }
+            BasicCard(
+                modifier=modifier.fillMaxWidth()
+            ) {
+                LazyVerticalGrid(
+                    modifier=modifier.padding(3.dp),
+                    columns = GridCells.Adaptive(minSize=45.dp),
+                    contentPadding= PaddingValues(horizontal=5.dp,vertical=1.dp)
+                ){
+                    //다른 item을 가져옴에 유의
+                    items(records){record->
+                        Text(record)
+                    }
+                }
+            }
+        }
+
+    }
 
 
 
